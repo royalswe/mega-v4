1660925a384b8159554e9abd03dfe3d4a8976707
royal <roynilsson87@gmail.com>
2026-02-04 23:53:12 +0100

    migrate fresh

---
+++ src/migrations/index.ts
@@ -0,0 +1,9 @@
+import * as migration_20260204_225133 from './20260204_225133';
+
+export const migrations = [
+  {
+    up: migration_20260204_225133.up,
+    down: migration_20260204_225133.down,
+    name: '20260204_225133'
+  },
+];
--- src/payload.config.ts
+++ src/payload.config.ts
@@ -5,7 +5,7 @@ import { buildConfig } from 'payload'
 import { fileURLToPath } from 'url'
 import sharp from 'sharp'
 
-import { migrations } from './migrations' // Payload now generates an index.ts here!
+import { migrations } from './migrations'
 import { Users } from './collections/Users'
 import { Media } from './collections/Media'
 import { Links } from './collections/Links'
