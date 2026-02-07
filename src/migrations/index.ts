1456cd3bc44bdeb08333a69c41e49618796f1dc0
royal <roynilsson87@gmail.com>
2026-02-07 23:45:35 +0100

    migrate

--- src/migrations/index.ts
+++ src/migrations/index.ts
@@ -1,9 +1,15 @@
 import * as migration_20260206_214812 from './20260206_214812';
+import * as migration_20260207_224524 from './20260207_224524';
 
 export const migrations = [
   {
     up: migration_20260206_214812.up,
     down: migration_20260206_214812.down,
-    name: '20260206_214812'
+    name: '20260206_214812',
+  },
+  {
+    up: migration_20260207_224524.up,
+    down: migration_20260207_224524.down,
+    name: '20260207_224524'
   },
 ];
